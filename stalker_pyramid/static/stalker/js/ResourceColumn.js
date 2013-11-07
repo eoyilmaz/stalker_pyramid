256878cfe5a4dd7144daa209fc422ae387b63185
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2013-11-07 11:22:20 +0200

    refactored the draw routine under a common function, so it is now possible to use it for both headers in 'w' zoom level and hopefully in others also.
    --HG--
    branch : develop

--- stalker_pyramid/static/stalker/js/ResourceColumn.js
+++ stalker_pyramid/static/stalker/js/ResourceColumn.js
@@ -24,43 +24,60 @@ define([
     'stalker/js/Resource'
 ], function (domConstruct, array, lang, locale, put, Resource) {
     // module:
-    //     ganttColumn
+    //     ResourceColumn
     // summary:
-    //     A dgrid column plugin that generates gantt chart time lines in a
-    //     column.
+    //     A dgrid column plugin that generates resource charts in a column.
 
-    function getColumnSetElement(element) {
-        // summary:
-        //     Finds the column set parent element of a given cell.
-        // element:
-        //     DomNode
-        // returns:
-        //     DomNode?
-        if (element) {
-            element = element.parentNode;
-            while (element && element.className !== "dgrid-column-set") {
-                element = element.parentNode;
-            }
+    var draw_header_cell = function (options) {
+        var parent = options.parent;
+        var start = options.start;
+        var end = options.end;
+        var scale = options.scale;
+        var step_size = options.step_size;
+        var step_unit = options.step_unit;
+        var period_unit = options.preiod_unit;
+        var height = options.height;
+        var top = options.top;
+        var format = options.format;
+
+        var original_start = moment(start).startOf('day');
+
+        var start_date = moment(start).startOf(period_unit).startOf('day');
+        var end_date = moment(end).endOf(period_unit).endOf('day');
+
+        // find the end of the first week
+        var period_start = moment(start_date.startOf(period_unit));
+        var period_end = moment(start_date.endOf(period_unit));
+        // create the first div elements using start_date and week_end
+
+        var parent_div = $($.parseHTML('<div class="headerCell"></div>'));
+        // fix scroll
+        parent_div.css({
+            width: Math.floor((end_date - start_date) / scale),
+            left: Math.floor((start_date - original_start) / scale),
+            height: height,
+            top: top,
+            position: 'absolute'
+        });
+        $(parent).append(parent_div);
+
+        var header_div_element;
+        // now wee need to iterate until the end_date is bigger than end
+        while (period_start < end_date) {
+            // create the first div elements using start_date and week_end
+            header_div_element = $($.parseHTML('<div class="headerCell center">' + period_start.format(format) + '</div>'));
+            header_div_element.css({
+                width: Math.floor((period_end - period_start) / scale),
+                left: Math.floor((period_start - start_date) / scale)
+            });
+            parent_div.append(header_div_element);
+            // go to next step
+            period_start.add(step_size, step_unit).startOf(step_unit);
+            period_end.add(step_size, step_unit).endOf(step_unit);
         }
-        return element;
-    }
+        return parent_div;
+    };
 
-
-    /**
-     * 
-     * 
-     * 
-     * Returns a formatted string
-     * 
-     * @param {number} start
-     *     the start of the range of a particular time box in
-     *     milliseconds
-     * @param {number} end
-     *     the end of the range of a particular time box in
-     *     milliseconds
-     * @returns {string}
-     * 
-     */
     var zoom_levels = {
 //        "h": {
 //            table_width: function (start, end) {
@@ -119,101 +136,97 @@ define([
             headers: [
                 {
                     draw: function (parent, start, end) {
-                        // draw itself
-
-                        // iterate from start to end day
-                        var scale = 21600000;
-
-                        var original_start = moment(start).startOf('day');
-                        var original_end = moment(start).endOf('day');
-
-                        var start_date = moment(start).startOf('month').startOf('day');
-                        var end_date = moment(end).endOf('month').endOf('day');
-
-                        // find the end of the first week
-                        var period_start = moment(start_date.startOf('month'));
-                        var period_end = moment(start_date.endOf('month'));
-                        // create the first div elements using start_date and week_end
-
-                        var parent_div = $($.parseHTML('<div class="headerCell"></div>'));
-                        // fix scroll
-                        parent_div.css({
-                            width: Math.floor((end_date - start_date) / scale),
-                            left: Math.floor((start_date - original_start) / scale), // offset as neccessary
+                        draw_header_cell({
+                            parent: parent,
+                            start: start,
+                            end: end,
+                            scale: 21600000,
+                            step_size: 1,
+                            step_unit: 'month',
+                            period_unit: 'month',
                             height: 26,
                             top: 0,
-                            position: 'absolute'
+                            format: 'MMM YYYY'
                         });
-                        $(parent).append(parent_div);
-                        $(parent).css({height: 26});
-
-                        var header_div_element;
-                        // now wee need to iterate until the end_date is bigger than end
-                        while (period_start < end_date) {
-                            // create the first div elements using start_date and week_end
-                            header_div_element = $($.parseHTML('<div class="headerCell center">' + period_start.format('MMM YYYY') + '</div>'));
-                            header_div_element.css({
-                                width: Math.floor((period_end - period_start) / scale),
-                                left: Math.floor((period_start - start_date) / scale)
-                            });
-                            parent_div.append(header_div_element);
-                            // go to next month
-                            period_start.add(1, 'month').startOf('month');
-                            period_end.add(1, 'month').endOf('month');
-                        }
-
                     }
                 },
                 {
                     draw: function (parent, start, end) {
-                        // draw itself
-
-                        // iterate from start to end day
-                        var scale = 21600000;
-
-                        var original_start = moment(start).startOf('day');
-                        var original_end = moment(end).endOf('day');
-
-                        var start_date = moment(start).startOf('isoweek').startOf('day');
-                        var end_date = moment(end).endOf('isoweek').endOf('day');
-
-                        // find the end of the first week
-                        var period_start = moment(start_date.startOf('isoweek'));
-                        var period_end = moment(start_date.endOf('isoweek'));
-                        // create the first div elements using start_date and week_end
-
-                        var parent_div = $($.parseHTML('<div class="headerCell"></div>'));
-                        // fix scroll
-                        parent_div.css({
-                            width: Math.floor((end_date - start_date) / scale),
-                            left: Math.floor((start_date - original_start) / scale), // offset as neccessary
+                        draw_header_cell({
+                            parent: parent,
+                            start: start,
+                            end: end,
+                            scale: 21600000,
+                            step_size: 7,
+                            step_unit: 'day',
+                            period_unit: 'isoweek',
                             height: 26,
                             top: 27,
-                            position: 'absolute'
+                            format: 'w'
                         });
-                        $(parent).append(parent_div);
-                        $(parent).css({height: 52});
-
-
-                        var header_div_element;
-                        // now wee need to iterate until the end_date is bigger than end
-                        while (period_start < end_date) {
-                            // create the first div elements using start_date and week_end
-                            header_div_element = $($.parseHTML('<div class="headerCell center">' + period_start.format('w') + '</div>'));
-                            header_div_element.css({
-                                width: Math.floor((period_end - period_start) / scale),
-                                left: Math.floor((period_start - start_date) / scale)
-                            });
-                            parent_div.append(header_div_element);
-                            // go to next step
-                            period_start.add(7, 'day');
-                            period_end.add(7, 'day');
-                        }
-
                     }
                 }
-            ]
-        },
+            ],
+            'chart': {
+                'draw': function(parent, data, start, end, scale) {
+                    // create a TimeLog instance
+                    var resource = new Resource(data);
+                    var original_start = moment(start).startOf('day');
+                    var original_end = moment(end).endOf('day');
+
+                    var start_date = moment(start).startOf('isoweek').startOf('day');
+                    var end_date = moment(end).endOf('isoweek').endOf('day');
+
+                    // iterate the weeks from start to end, choose the week start and
+                    // dates correctly
+                    var range_start = moment(start_date).startOf('isoweek');
+                    var range_end = moment(range_start).add(7, 'days');
+
+                    var total_logged_millies;
+                    var weekly_millies_possible = 183600000; // 51 hours for anima
+                    var weekly_log_bar;
+                    var weekly_log_bar_layout_div;
+
+                    var parent_div = $($.parseHTML('<div class="headerCell"></div>'));
+                    parent_div.css({
+                        width: Math.floor((end_date - start_date) / scale),
+                        left: Math.floor((start_date - original_start) / scale), // offset as neccessary
+                        height: 24,
+                        position: 'absolute'
+                    });
+                    $(parent).append(parent_div);
+
+                    var Math_floor = Math.floor;
+                    var added_first_time_log = false;
+                    var resource_count = resource.resource_count;
+
+                    while (range_start < end_date) {
+                        total_logged_millies = resource.total_logged_milliseconds(+range_start, +range_end);
+                        // draw a div at that range with the height of total_logged_millies
+                        if (total_logged_millies > 0 || added_first_time_log) {
+                            added_first_time_log = true;
+                            weekly_log_bar_layout_div = $($.parseHTML('<div class="log_bar layout"></div>'));
+                            weekly_log_bar_layout_div.css({
+                                left: Math_floor((range_start - start_date) / scale),
+                                width: Math_floor((range_end - range_start) / scale) - 1 // just to give a feeling of border
+                            });
+
+                            weekly_log_bar = $($.parseHTML('<div class="log_bar timeLog"></div>'));
+                            weekly_log_bar.css({
+                                height: Math_floor(total_logged_millies / weekly_millies_possible * 22 / resource_count)// weekly millies possible
+                            });
+                            weekly_log_bar_layout_div.append(weekly_log_bar);
+                            parent_div.append(
+                                weekly_log_bar_layout_div
+                            );
+                        }
+                        // get the new start and end values
+                        range_start.add(7, 'day');
+                        range_end.add(7, 'day');
+                    }
+                }
+            }
+        }
 //        "m": { // every week in a box
 //        },
 //        "q": {},
@@ -293,80 +306,12 @@ define([
          *     DomNode
          */
         column.renderCell = function (data, value, td) {
-//            console.log('ResourceColumn.column.renderCell start');
             // IE < 8 receive the inner padding node, not the td directly
             var cell = td.tagName === "TD" ? td : td.parentNode;
 
-            // Add empty content to the cell to avoid it collapsing in IE
-            //td.innerHTML = "&nbsp;";
-
-            // create a TimeLog instance
-            var resource = new Resource(data);
-
-            // Ensure the start time is always milliseconds since epoch
-            // and not a Date object
-
-            var original_start = moment(column.start).startOf('day');
-            var original_end = moment(column.end).endOf('day');
-
-            var start_date = moment(column.start).startOf('isoweek').startOf('day');
-            var end_date = moment(column.end).endOf('isoweek').endOf('day');
-
-            // This is the number of milliseconds per pixel rendered
             var scale = zoom_levels[column.scale].scale;
-
-            // iterate the weeks from start to end, choose the week start and
-            // dates correctly
-            var range_start = moment(start_date).startOf('isoweek');
-            var range_end = moment(range_start).add(7, 'days');
-
-            var total_logged_millies;
-            var weekly_millies_possible = 183600000; // 51 hours for anima
-            var weekly_log_bar;
-            var weekly_log_bar_layout_div;
-            var td_jq = $(td);
-
-            var parent_div = $($.parseHTML('<div class="headerCell"></div>'));
-            parent_div.css({
-                width: Math.floor((end_date - start_date) / scale),
-                left: Math.floor((start_date - original_start) / scale), // offset as neccessary
-                height: 24,
-                position: 'absolute'
-            });
-            td_jq.append(parent_div);
-
-
-            var Math_floor = Math.floor;
-
-            var added_first_time_log = false;
-            var today = moment(new Date()).startOf('day');
-
-            var resource_count = resource.resource_count;
-
-            while (range_start < end_date) {
-                total_logged_millies = resource.total_logged_milliseconds(+range_start, +range_end);
-                // draw a div at that range with the height of total_logged_millies
-                if (total_logged_millies > 0 || added_first_time_log) {
-                    added_first_time_log = true;
-                    weekly_log_bar_layout_div = $($.parseHTML('<div class="log_bar layout"></div>'));
-                    weekly_log_bar_layout_div.css({
-                        left: Math_floor((range_start - start_date) / scale),
-                        width: Math_floor((range_end - range_start) / scale) - 1 // just to give a feeling of border
-                    });
-
-                    weekly_log_bar = $($.parseHTML('<div class="log_bar timeLog"></div>'));
-                    weekly_log_bar.css({
-                        height: Math_floor(total_logged_millies / weekly_millies_possible * 22 / resource_count)// weekly millies possible
-                    });
-                    weekly_log_bar_layout_div.append(weekly_log_bar);
-                    parent_div.append(
-                        weekly_log_bar_layout_div
-                    );
-                }
-                // get the new start and end values
-                range_start.add(7, 'day');
-                range_end.add(7, 'day');
-            }
+            // render cells
+            zoom_levels[column.scale].chart.draw(td, data, column.start, column.end, scale);
 
             // render today
             render_today(td, column.start, zoom_levels[column.scale].scale);
@@ -440,9 +385,14 @@ define([
             // get element width
             // render headers
             var i;
-            for (i = 0; i < zoom_levels[column.scale].headers.length; i += 1) {
+            var header_count = zoom_levels[column.scale].headers.length;
+            for (i = 0; i < header_count; i += 1) {
                 zoom_levels[column.scale].headers[i].draw(th, column.start, column.end);
             }
+            // extend the height of the table header
+            $(th).css({
+                height: header_count * 27 // 27 px each
+            });
 
             // render today
             render_today(th, column.start, zoom_levels[column.scale].scale);
