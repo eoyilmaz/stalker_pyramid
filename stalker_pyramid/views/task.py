c5f13209f8ebdc2e9fb65f9839bbeb849a76b4c4
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2014-06-12 12:12:33 +0300

    fixed views.task.create_task() for priority parameter not being an integer
    --HG--
    branch : develop

--- stalker_pyramid/views/task.py
+++ stalker_pyramid/views/task.py
@@ -2563,7 +2563,7 @@ def create_task(request):
         responsible_ids = get_multi_integer(request, 'responsible_ids[]')
         responsible = User.query.filter(User.id.in_(responsible_ids)).all()
 
-    priority = request.params.get('priority', 500)
+    priority = int(request.params.get('priority', 500))
 
     code = request.params.get('code', '')
     entity_type = request.params.get('entity_type')
